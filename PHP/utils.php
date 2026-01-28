35da5b024965cc90f6ae3a01a22c8c9eacd59803
FelicianNek <felifelix04@gmail.com>
2026-01-29 00:35:53 +0100

    buonanotte

--- PHP/utils.php
+++ PHP/utils.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use Pelago\Emogrifier\CssInliner;
 
-const PROJECT_ROOT = "/Amici-per-Sempre";
+const PROJECT_ROOT = "/tec-web";
 
 // Funzione per l'avvio della sessione se non già avviata
 function ensure_session() {
