fa1f4fcf9b03b0bbd94b2b8a858a2e19ffabe2c6
FelicianNek <felifelix04@gmail.com>
2026-02-02 18:14:19 +0100

    popolamento

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ http_response_code(404);
 $dati = [
     '{{current-page}}' => 'Errore 404',
     '{{page-description}}' => 'Errore 404. La pagina richiesta non è disponibile. Visita Amici per Sempre Rifugio Padova per conoscere cani e gatti in adozione.',
-    '{{page-keywords}}' => 'amici per sempre, errore, pagina non trovata, 404',
+    '{{page-keywords}}' => 'Amici per Sempre, errore, pagina non trovata, 404',
     '{{current-js}}' => '',
 ];
 
